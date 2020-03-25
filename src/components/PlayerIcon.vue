8cc42d1554bf99b683dd24843ebdf3cd3fb5a91d
Simon Heiss <simonheiss87@gmail.com>
2020-03-25 07:28:48 +0100

    Revert "Revert "playerlist 2 rows again""
    This reverts commit 42bb938f

--- src/components/PlayerIcon.vue
+++ src/components/PlayerIcon.vue
@@ -28,8 +28,8 @@ export default class PlayerIcon extends Vue {
 
 <style lang="scss">
 .race-icon {
-  width: 42px;
-  height: 42px;
+  width: 24px;
+  height: 24px;
   background-position: center;
   background-size: cover;
 }
